646bf4d28e18120f9fa199793ab06dfd4f27d992
tunatone0111 <tunatone.0111@gmail.com>
2020-06-07 03:44:48 +0900

    06070344

--- VHW5/functionunit.v
+++ VHW5/functionunit.v
@@ -3,69 +3,69 @@ module funcunit(FS, A, B, D, V, C, N, Z, CLK, RESET);
 input CLK, RESET;
 input [3:0] FS;
 input [15:0] A, B;
-output V, C, N, Z;
+reg [16:0] temp;
+output reg V, C, N, Z;
 output reg [15:0] D;
 
 always @(posedge CLK) begin
     if(RESET == 1) D = 16'h0000;
     else begin
-        V
-        C
-        N
-        Z
         case(FS)
-            2'b0000:begin
-                D <= A;
+            4'b0000:begin
+                {C, D} <= A;
             end
-            2'b0001:begin
-                D <= A+1;
+            4'b0001:begin
+                {C, D} <= A + 1;
             end
-            2'b0010:begin
-                D <= A+B;
+            4'b0010:begin
+                {C, D} <= A+B;
             end
-            2'b0011:begin
-                D <= A+B+1;
+            4'b0011:begin
+                {C, D} <= A+B+1;
             end
-            2'b0100:begin
-                //A+~B
-                D <= A+~B;
+            4'b0100:begin
+                {C, D} <= A+~B;
             end
-            2'b0101:begin
-                //A+~B+1
-                D <= A+~B+1;
+            4'b0101:begin
+                {C, D} <= A+~B+1;
             end
-            2'b0110:begin
-                //A-1
-                D <= A-1;
+            4'b0110:begin
+                {C, D} <= A-1;
             end
-            2'b0111:begin
-                D <= A
+            4'b0111:begin
+                {C, D} <= A
             end
-            2'b1000:begin
+            4'b1000:begin
                 D <= A & B;
             end
-            2'b1001:begin
+            4'b1001:begin
                 D <= A | B;
             end
-            2'b1010:begin
+            4'b1010:begin
                 D <= A ^ B;
             end
-            2'b1011:begin
+            4'b1011:begin
                 D <= ~A;
             end
-            2'b1100:begin
+            4'b1100:begin
                 D <= B;
             end
-            2'b1101:begin
+            4'b1101:begin
                 D <= B>>1;
             end
-            2'b1110:begin
+            4'b1110:begin
                 D <= B<<1;
             end
-            2'b1111:begin
+            4'b1111:begin
                 D <= D;
             end
         endcase
+
+        if(D == 4'h0000) Z = 1;
+        else Z = 0;
+
+        N <= D[15];
+        V <= D[15] & C;
     end
 end
 
